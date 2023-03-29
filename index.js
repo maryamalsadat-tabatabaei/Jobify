4bf5b761816362cdec5866182fb08705aac47fe9
Maryam (Maryamalsadat) Tabatabaei <87692864+maryamsadat-tabatabaei@users.noreply.github.com>
2023-03-29 11:08:44 +0430

    initial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("welconme");
+});
+
+const PORT = process.env.PORT || 8000;
+
+app.listen(PORT);
